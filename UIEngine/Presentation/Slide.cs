c6dea550c73afe5dbb92bccc54c276edc8c3da8c
philly <sonagi.philly@gmail.com>
2016-10-13 22:14:12 +0900

    Added WBStool file - plan for develop.

--- UIEngine/Presentation/Slide.cs
+++ UIEngine/Presentation/Slide.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace UIEngine
+namespace UIEngine.Presentation
 {
-    public interface IAnimation
+    public class Slide
     {
-        void Play();
+
     }
 }
